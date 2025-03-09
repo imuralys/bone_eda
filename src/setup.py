f800b7fcc24f8e115c4d9002e3417614a57ebe93
Alys <nnhn2953@gmail.com>
2025-03-09 22:12:45 +0700

    setup

---
+++ src/setup.py
@@ -0,0 +1,25 @@
+import os
+import json
+import kaggle
+
+# Kiểm tra và tạo thư mục .kaggle
+kaggle_dir = os.path.expanduser('~/.kaggle')
+os.makedirs(kaggle_dir, exist_ok=True)
+
+# Tạo config
+config = {
+    "username": "hiimuralys",
+    "key": "75ae8a7c35a113a06341e1c34fc3241a"  # Thay bằng key mới
+}
+
+# Lưu config
+with open(os.path.join(kaggle_dir, 'kaggle.json'), 'w') as f:
+    json.dump(config, f)
+
+# Đặt quyền truy cập
+os.chmod(os.path.join(kaggle_dir, 'kaggle.json'), 600)
+
+# Tải dataset
+kaggle.api.dataset_download_files('thanhngan123/btxrd-data', 
+                                 path='data',
+                                 unzip=True)
